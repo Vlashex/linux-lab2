957e3c5688831a25561fa1df6110c1ca6a42e8ea
Vlashex <rmaynavine@gmail.com>
2025-10-17 19:05:00 +0300

    zad3: Add \q command to exit the shell loop

--- main.cpp
+++ main.cpp
@@ -3,10 +3,18 @@
 
 int main() {
     std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
     std::string input;
+
+    std::cerr << "$ ";
     while (std::getline(std::cin, input)) {
+        if (input == "\\q") {
+            break;
+        }
+
         std::cout << input << '\n';
+        std::cerr << "$ ";
     }
 
     return 0;
